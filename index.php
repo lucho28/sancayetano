cbd02e6d87495add8a1ff3defe41ca0355d153ee
lucho28 <luedugonzalez@gmail.com>
2023-12-04 20:47:02 -0300

    cambiamos a bootstrap

--- index.php
+++ index.php
@@ -1,30 +1,65 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>San Cayetano</title>
     <link rel="stylesheet" href="estilos.css">
-    <title>Document</title>
-</head>
-<body>
-<header>
-        <nav>
-            <ul>
-                <li><a href="index.php">Inicio</a></li>
-                <li><a href="metalmecanica.php">Metalmecanica</a></li>
-                <li><a href="">Industrializacion Lacteos</a></li>
-                <li><a href="">Agronegocios</a></li>
-                <li><a href="">Multinegocios</a></li>
-                <li><a href="">Hilados y bolsas</a></li>
-                <li><a href="">Refrigeracion</a></li>
-                <li><a href="">Comercializacion de vehiculos</a></li>
-            </ul>
-        </nav>
-    </header>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body class="bg-secondary">
+    
+  <header>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="img/logo.jpeg" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#menu" aria-controls="navbarSupportedContent" 
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="menu">
+                <ul class="navbar-nav md-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="metalmecanica.php">Metalmecanica</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Lacteos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Agronegocios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Multinegocios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Hilados y bolsas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Refrigeracion</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Comercializacion de vehiculos</a>
+                    </li>
+                </ul>
+            </div>
+    </nav>
+
+  </header>
+    
     
 
-    <footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+
+  <footer>
         <h3>Centro de desarrollo socio-productivo San Cayetano</h3>
     </footer>
-</body>
 </html>
---
+++ index3.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
+    <title>Document</title>
+</head>
+<body>
+<header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="metalmecanica.php">Metalmecanica</a></li>
+                <li><a href="">Industrializacion Lacteos</a></li>
+                <li><a href="">Agronegocios</a></li>
+                <li><a href="">Multinegocios</a></li>
+                <li><a href="">Hilados y bolsas</a></li>
+                <li><a href="">Refrigeracion</a></li>
+                <li><a href="">Comercializacion de vehiculos</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+
+    <footer>
+        <h3>Centro de desarrollo socio-productivo San Cayetano</h3>
+    </footer>
+</body>
+</html>
