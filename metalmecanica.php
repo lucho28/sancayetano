318ffcadd73556170ff1664b8438ab7f40b41b87
lucho28 <luedugonzalez@gmail.com>
2023-12-06 15:15:45 -0300

    fondo blanco

--- metalmecanica.php
+++ metalmecanica.php
@@ -8,11 +8,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
 
-  <body class="bg-secondary">
+  <body class="bg-wite">
     
   <header>
   
-  <nav class="navbar navbar-expand-md navbar-dark">
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" id="logo" href="#">
             <img src="img/logo.png" alt="">
