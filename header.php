7efeea3ef26bbec22447599c7c6c59d643d89de0
lucho28 <luedugonzalez@gmail.com>
2023-12-14 18:47:00 -0300

    intento de header general

---
+++ header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>  
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" id="logo" href="#">
+            <img src="img/logo.png" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#menu" aria-controls="navbarSupportedContent" 
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="menu">
+                <ul class="navbar-nav md-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.php">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="metalmecanica.php">Metalmecanica</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="lacteos.php">Lacteos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Agronegocios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Multinegocios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Hilados y bolsas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Refrigeracion</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Comercializacion de vehiculos</a>
+                    </li>
+                </ul>
+            </div>
+    </nav>
+    </div>
+    
+    </header>
+</body>
+</html>
