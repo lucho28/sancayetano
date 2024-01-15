830478adca1f3ec1d8bd3677813185a5499b5363
lucho28 <luedugonzalez@gmail.com>
2024-01-15 12:32:27 -0300

    header

--- index.php
+++ index.php
@@ -9,7 +9,9 @@
   </head>
   
   <header>
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    
+
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
         <div class="container-fluid">
                     
         <a class="navbar-brand" id="logo" href="#">
@@ -23,12 +25,38 @@
             </button>
             <div class="collapse navbar-collapse" id="menu">
 
-                <?php include("header.php") ?>
-            
-            </div>
+                <ul class="navbar-nav md-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.php">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="metalmecanica.php">Metalmecanica</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="lacteos.php">Lacteos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Agronegocios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Multinegocios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Hilados y bolsas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Refrigeracion</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Comercializacion de vehiculos</a>
+                    </li>
+                </ul>
+                </div>
 
         </div>
     </nav>
+            
+            
   </header>
   
   
