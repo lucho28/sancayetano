255d5926ccfe8a2a45681bff38454c0145338379
lucho28 <luedugonzalez@gmail.com>
2024-01-22 10:55:09 -0300

    hhhh

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="es">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,89 +8,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   
-  <header>
-    
-
-  <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-        <div class="container-fluid">
-                    
-        <a class="navbar-brand" id="logo" href="#">
-            <img src="img/logo.png" alt="">
-            </a>
-            <button class="navbar-toggler" type="button" 
-            data-bs-toggle="collapse" 
-            data-bs-target="#menu" aria-controls="navbarSupportedContent" 
-            aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="menu">
-
-<<<<<<< HEAD
-            <ul class="navbar-nav md-auto">
-=======
-                <ul class="navbar-nav md-auto">
->>>>>>> 830478adca1f3ec1d8bd3677813185a5499b5363
-                    <li class="nav-item">
-                        <a class="nav-link active" href="index.php">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="metalmecanica.php">Metalmecanica</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="lacteos.php">Lacteos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Agronegocios</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Multinegocios</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Hilados y bolsas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Refrigeracion</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Comercializacion de vehiculos</a>
-                    </li>
-                </ul>
-<<<<<<< HEAD
-            
-            </div>
-=======
-                </div>
->>>>>>> 830478adca1f3ec1d8bd3677813185a5499b5363
-
-        </div>
-    </nav>
-            
-            
-  </header>
-  <div>
-
-    
-  </header>
-    
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus iure quas tempore dignissimos debitis quam temporibus aliquid distinctio libero dolorum quis voluptates magnam, beatae veniam facere cupiditate unde tempora expedita.</p>
-
-<h1> QUE ONDA !!</h1>
-</div>
-  
-  
   <body class="bg-secondary">
-    
-  
-  
-  
-  
+    <div class="boxed_wrapper">
+    <?php include 'includes/header.php'; ?>
 
-    
+    </div>
+     
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
   </body>
 
+  
   <footer class="fixed-bottom footer-dark bg-dark text-light">
         <h3>© 2024 | Nueve de Julio - Municipalidad - Centro de Desarrollo Socio Productivo San Cayetano </h3>
-    </footer>
-</html>
+  </footer>
+
+  </html>
