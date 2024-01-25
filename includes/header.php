29f9d3cb101ecf56b252561df7102f7fb0b834ff
lucho28 <luedugonzalez@gmail.com>
2024-01-25 08:22:32 -0300

    asd

--- includes/header.php
+++ includes/header.php
@@ -1,58 +1,47 @@
 <header>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-    <div class="container-fluid">                 
-          <a class="navbar-brand" id="logo" href="index.php">
-              <img src="images/logo.png" alt="" >
-              </a>
-              <button class="navbar-toggler" type="button" 
-              data-bs-toggle="collapse" 
-              data-bs-target="#menu" aria-controls="navbarSupportedContent" 
-              aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-              </button>
+        <div class="container-fluid">                 
+            <a class="navbar-brand" id="logo" href="index.php">
+                <img src="images/logo.png" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" 
+                data-bs-toggle="collapse" 
+                data-bs-target="#menu" aria-controls="navbarSupportedContent" 
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-  
-                  <ul class="navbar-nav md-auto">
-                      <li class="nav-item">
-                          <a class="nav-link" href="index.php">INICIO</a>
-                      </li>
-                      <li class="nav-item">
-                          <a class="nav-link" href="#">¿QUIENES SOMOS?</a>
-                      </li>
-                      <li class="nav-item">
-                          <a class="nav-link" href="#">CAPACITACION</a>
-                      </li>
+            <div class="collapse navbar-collapse" id="menu">
+                <ul class="navbar-nav md-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">INICIO</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">¿QUIENES SOMOS?</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">CAPACITACION</a>
+                    </li>
 
-                      <li class="nav-item dropdown">
+                    <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        CADENAS PRODUCTIVAS
+                            CADENAS PRODUCTIVAS
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="metalmecanica.php">METALMECANICA</a>
                             <a class="dropdown-item" href="lacteos.php">LACTEOS</a>
-                            <!--
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                            -->
                         </div>    
-                      </li>
-                      
-                  </ul>
-                    
-                    
-                    
-               
-  
+                    </li>
+                </ul>
+
+                
             </div>
             <div class="ml-auto d-flex">
-                <form class="my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
-                </form>
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-            </div>
+                    <form class="my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+                    </form>
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+                </div>
         </div>
-      </nav>
-              
-              
+    </nav>
 </header>
